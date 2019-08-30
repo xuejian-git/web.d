59e8269eb95c8e298008453800e29528eceef60b
Shiny-Man <239194147@qq.com>
2019-08-30 14:48:21 +0800

    webd

---
+++ src/core/DataBase.cpp
@@ -0,0 +1,9 @@
+/**********************************************************
+ *   Author          : Apriluestc
+ *   Email           : 13669186256@163.com
+ *   Last modified   : 2019-08-30 14:48
+ *   Filename        : DataBase.cpp
+ *   Description     : 
+ * *******************************************************/
+
+#include "DataBase.h"
---
+++ src/core/DataBase.h
@@ -0,0 +1,14 @@
+/**********************************************************
+ *   Author          : Apriluestc
+ *   Email           : 13669186256@163.com
+ *   Last modified   : 2019-08-30 14:47
+ *   Filename        : DataBase.h
+ *   Description     : 
+ * *******************************************************/
+
+#ifndef INCLUDE_DATABASE_H
+#define INCLUDE_DATABASE_H
+
+class DataBase {};
+
+#endif
